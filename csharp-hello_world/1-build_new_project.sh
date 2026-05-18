695e243bc3c27f61793d3338e523a36b14bb2e32
audranALU <a.ndayiseng@alustudent.com>
2026-05-18 11:10:17 +0200

    Update 1-build_new_project.sh

--- csharp-hello_world/1-build_new_project.sh
+++ csharp-hello_world/1-build_new_project.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-dotnet new console -o 2-new_project
-dotnet build 2-new_project
-dotnet run --project 2-new_project
+dotnet new console -o 1-new_project
+dotnet build 1-new_project
