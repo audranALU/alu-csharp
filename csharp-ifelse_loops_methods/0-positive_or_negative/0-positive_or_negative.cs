330825a0c1f0e726d78c07aae6f205d14cf4d2ea
audranALU <a.ndayiseng@alustudent.com>
2026-05-18 12:48:55 +0200

    Fix formatting and remove unnecessary newline

--- csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
+++ csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
@@ -4,7 +4,6 @@ class Program
     static void Main(string[] args)
     {
         int number = new Random().Next(-10, 10);
-
         if (number > 0)
             Console.WriteLine("{0} is positive", number);
         else if (number == 0)
