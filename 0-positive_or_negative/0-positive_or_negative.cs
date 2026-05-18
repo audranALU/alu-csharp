49dc22bba1facee9074b9928c7dbd8a323e2ade8
audranALU <a.ndayiseng@alustudent.com>
2026-05-18 12:40:04 +0200

    Add program to check if a number is positive, negative, or zero

--- 0-positive_or_negative/0-positive_or_negative.cs
+++ 0-positive_or_negative/0-positive_or_negative.cs
@@ -1,1 +1,16 @@
+using System;
 
+class Program
+{
+    static void Main(string[] args)
+    {
+        int number = new Random().Next(-10, 10);
+
+        if (number > 0)
+            Console.WriteLine("{0} is positive", number);
+        else if (number == 0)
+            Console.WriteLine("{0} is zero", number);
+        else
+            Console.WriteLine("{0} is negative", number);
+    }
+}
