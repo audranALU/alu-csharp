947359621b80c24925eea91b5bed0082cb09caa6
audranALU <a.ndayiseng@alustudent.com>
2026-05-20 09:46:50 +0200

    Add number printing program for 00 to 99
    Implement a program to print numbers from 00 to 99 with formatting.

---
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,19 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i <= 99; i++)
+        {
+            if (i < 99)
+            {
+                Console.Write("{0:D2}, ", i);
+            }
+            else
+            {
+                Console.WriteLine("{0:D2}", i);
+            }
+        }
+    }
+}
