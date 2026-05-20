0501e29f05a96b28d0b604841fbbfdf3b42311b7
audranALU <a.ndayiseng@alustudent.com>
2026-05-20 22:35:49 +0200

    Implement CreatePrint method to generate and print list

---
+++ csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int>? CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+
+        List<int> myList = new List<int>();
+
+        for (int i = 0; i < size; i++)
+        {
+            myList.Add(i);
+            Console.Write(myList[i]);
+            if (i < size - 1)
+                Console.Write(" ");
+        }
+        Console.WriteLine();
+
+        return myList;
+    }
+}
