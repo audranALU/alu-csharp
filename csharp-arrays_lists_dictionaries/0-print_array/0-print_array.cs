905de604ec206e34716b1a5785278814e8f5f340
audranALU <a.ndayiseng@alustudent.com>
2026-05-20 22:25:31 +0200

    Implement array creation and printing method

---
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,26 @@
+using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+
+        int[] arr = new int[size];
+
+        for (int i = 0; i < size; i++)
+        {
+            arr[i] = i;
+            Console.Write(arr[i]);
+            if (i < size - 1)
+                Console.Write(" ");
+        }
+        Console.WriteLine();
+
+        return arr;
+    }
+}
