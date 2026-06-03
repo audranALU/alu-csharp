2df75b5fd66f481527fa12d861c1e3dc728f8610
audranALU <a.ndayiseng@alustudent.com>
2026-06-03 10:38:18 +0200

    Implement Sum method to add unique integers

---
+++ csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+
+class List
+{
+	// A HashSet ignores repeats, so Add returns true only the first time a value appears.
+	public static int Sum(System.Collections.Generic.List<int> myList)
+	{
+		var seen = new HashSet<int>();
+		int total = 0;
+		foreach (int value in myList)
+			if (seen.Add(value))
+				total += value;
+		return total;
+	}
+}
