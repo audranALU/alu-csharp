2da33b5fb7d053479882245c44895f34cf6e0e39
audranALU <a.ndayiseng@alustudent.com>
2026-06-03 12:20:05 +0200

    Refactor Sum method to improve readability

--- csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
+++ csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -1,15 +1,19 @@
 using System;
 using System.Collections.Generic;
+
 class List
 {
-	// A HashSet ignores repeats, so Add returns true only the first time a value appears.
-	public static int Sum(System.Collections.Generic.List<int> myList)
-	{
-		var seen = new HashSet<int>();
-		int total = 0;
-		foreach (int value in myList)
-			if (seen.Add(value))
-				total += value;
-		return total;
-	}
+    public static int Sum(List<int> myList)
+    {
+        HashSet<int> seen = new HashSet<int>();
+        int sum = 0;
+
+        foreach (int n in myList)
+        {
+            if (seen.Add(n))
+                sum += n;
+        }
+
+        return sum;
+    }
 }
