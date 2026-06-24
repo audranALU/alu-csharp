42d861b5d757270b3c1b793fa9a21dedbd7f9f9c
audranALU <a.ndayiseng@alustudent.com>
2026-06-24 23:19:52 +0200

    Add unit tests for Operations.Add method

---
+++ csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace MyMath.Tests
+{
+    [TestFixture]
+    public class OperationsTests
+    {
+        [TestCase(1, 2, 3)]
+        [TestCase(-1, 1, 0)]
+        [TestCase(0, 0, 0)]
+        [TestCase(-5, -5, -10)]
+        public void Add_ReturnsCorrectSum(int a, int b, int expected)
+        {
+            Assert.AreEqual(expected, Operations.Add(a, b));
+        }
+    }
+}
