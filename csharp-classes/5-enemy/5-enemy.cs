9ad3ee352832872840e16bb8930545e780eba92f
audranALU <a.ndayiseng@alustudent.com>
2026-06-24 22:54:41 +0200

    Create 5-enemy.cs

---
+++ csharp-classes/5-enemy/5-enemy.cs
@@ -0,0 +1,62 @@
+using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Represents a zombie.
+    /// </summary>
+    public class Zombie
+    {
+        // health of the zombie
+        private int health;
+        // name of the zombie
+        private string name = "(No name)";
+
+        /// <summary>
+        /// Gets or sets the name of the zombie.
+        /// </summary>
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Zombie"/> class.
+        /// </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Zombie"/> class.
+        /// </summary>
+        /// <param name="value">Health value, must be greater than or equal to 0.</param>
+        /// <exception cref="ArgumentException">Thrown when value is less than 0.</exception>
+        public Zombie(int value)
+        {
+            if (value < 0)
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            health = value;
+        }
+
+        /// <summary>
+        /// Returns the health value of the zombie.
+        /// </summary>
+        /// <returns>The health value.</returns>
+        public int GetHealth()
+        {
+            return health;
+        }
+
+        /// <summary>
+        /// Returns a string representation of the zombie.
+        /// </summary>
+        /// <returns>Formatted string with name and health.</returns>
+        public override string ToString()
+        {
+            return $"Zombie Name: {name} / Total Health: {health}";
+        }
+    }
+}
