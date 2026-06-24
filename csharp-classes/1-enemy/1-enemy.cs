d4f859e04d780c38d6bd15d521c1dfc2242c11ae
audranALU <a.ndayiseng@alustudent.com>
2026-06-24 22:48:38 +0200

    Add Zombie class with health property

---
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,19 @@
+namespace Enemies
+{
+    /// <summary>
+    /// Represents a zombie.
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary>Health of the zombie.</summary>
+        public int health;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Zombie"/> class.
+        /// </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+    }
+}
